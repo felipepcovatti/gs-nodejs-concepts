488f4816535f0ac7c0b1d0f27c729e97c7cddce8
Felipe P Covatti <felipepcovatti@gmail.com>
2020-06-19 22:07:10 -0300

    Set post request responsible for adding a new repo

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +14,16 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+  repositories.push(newRepo);
+  response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
